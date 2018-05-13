4af1b166e0024183d4a99918a6d1a5b0922ad2a8
Justin Shenk <shenk.justin@gmail.com>
2018-05-13 17:42:16 +0200

    Update formatting

--- src/estimator.py
+++ src/estimator.py
@@ -40,12 +40,12 @@ class Human:
 
     def add_pair(self, pair):
         self.pairs.append(pair)
-        self.body_parts[pair.part_idx1] = BodyPart(Human._get_uidx(pair.part_idx1, pair.idx1),
-                                                   pair.part_idx1,
-                                                   pair.coord1[0], pair.coord1[1], pair.score)
-        self.body_parts[pair.part_idx2] = BodyPart(Human._get_uidx(pair.part_idx2, pair.idx2),
-                                                   pair.part_idx2,
-                                                   pair.coord2[0], pair.coord2[1], pair.score)
+        self.body_parts[pair.part_idx1] = BodyPart(
+            Human._get_uidx(pair.part_idx1, pair.idx1), pair.part_idx1,
+            pair.coord1[0], pair.coord1[1], pair.score)
+        self.body_parts[pair.part_idx2] = BodyPart(
+            Human._get_uidx(pair.part_idx2, pair.idx2), pair.part_idx2,
+            pair.coord2[0], pair.coord2[1], pair.score)
         self.uidx_list.add(Human._get_uidx(pair.part_idx1, pair.idx1))
         self.uidx_list.add(Human._get_uidx(pair.part_idx2, pair.idx2))
 
@@ -84,7 +84,8 @@ class BodyPart:
         return CocoPart(self.part_idx)
 
     def __str__(self):
-        return 'BodyPart:%d-(%.2f, %.2f) score=%.2f' % (self.part_idx, self.x, self.y, self.score)
+        return 'BodyPart:%d-(%.2f, %.2f) score=%.2f' % (self.part_idx, self.x,
+                                                        self.y, self.score)
 
 
 class PoseEstimator:
@@ -98,13 +99,12 @@ class PoseEstimator:
     Part_Count_Threshold = 4
     Part_Score_Threshold = 4.5
 
-    PartPair = namedtuple('PartPair', [
-        'score',
-        'part_idx1', 'part_idx2',
-        'idx1', 'idx2',
-        'coord1', 'coord2',
-        'score1', 'score2'
-    ], verbose=False)
+    PartPair = namedtuple(
+        'PartPair', [
+            'score', 'part_idx1', 'part_idx2', 'idx1', 'idx2', 'coord1',
+            'coord2', 'score1', 'score2'
+        ],
+        verbose=False)
 
     def __init__(self):
         pass
@@ -113,7 +113,8 @@ class PoseEstimator:
     def non_max_suppression(plain, window_size=3, threshold=NMS_Threshold):
         under_threshold_indices = plain < threshold
         plain[under_threshold_indices] = 0
-        return plain * (plain == maximum_filter(plain, footprint=np.ones((window_size, window_size))))
+        return plain * (plain == maximum_filter(
+            plain, footprint=np.ones((window_size, window_size))))
 
     @staticmethod
     def estimate(heat_mat, paf_mat):
@@ -123,34 +124,40 @@ class PoseEstimator:
             paf_mat = np.rollaxis(paf_mat, 2, 0)
 
         if PoseEstimator.heatmap_supress:
-            heat_mat = heat_mat - heat_mat.min(axis=1).min(axis=1).reshape(19, 1, 1)
-            heat_mat = heat_mat - heat_mat.min(axis=2).reshape(19, heat_mat.shape[1], 1)
+            heat_mat = heat_mat - heat_mat.min(axis=1).min(axis=1).reshape(
+                19, 1, 1)
+            heat_mat = heat_mat - heat_mat.min(axis=2).reshape(
+                19, heat_mat.shape[1], 1)
 
         if PoseEstimator.heatmap_gaussian:
             heat_mat = gaussian_filter(heat_mat, sigma=0.5)
 
         if PoseEstimator.adaptive_threshold:
-            _NMS_Threshold = max(np.average(heat_mat) * 4.0, PoseEstimator.NMS_Threshold)
+            _NMS_Threshold = max(
+                np.average(heat_mat) * 4.0, PoseEstimator.NMS_Threshold)
             _NMS_Threshold = min(_NMS_Threshold, 0.3)
         else:
             _NMS_Threshold = PoseEstimator.NMS_Threshold
 
         # extract interesting coordinates using NMS.
-        coords = []     # [[coords in plane1], [....], ...]
+        coords = []  # [[coords in plane1], [....], ...]
         for plain in heat_mat[:-1]:
             nms = PoseEstimator.non_max_suppression(plain, 5, _NMS_Threshold)
             coords.append(np.where(nms >= _NMS_Threshold))
 
         # score pairs
         pairs_by_conn = list()
-        for (part_idx1, part_idx2), (paf_x_idx, paf_y_idx) in zip(CocoPairs, CocoPairsNetwork):
+        for (part_idx1, part_idx2), (paf_x_idx, paf_y_idx) in zip(
+                CocoPairs, CocoPairsNetwork):
             pairs = PoseEstimator.score_pairs(
-                part_idx1, part_idx2,
-                coords[part_idx1], coords[part_idx2],
-                paf_mat[paf_x_idx], paf_mat[paf_y_idx],
+                part_idx1,
+                part_idx2,
+                coords[part_idx1],
+                coords[part_idx2],
+                paf_mat[paf_x_idx],
+                paf_mat[paf_y_idx],
                 heatmap=heat_mat,
-                rescale=(1.0 / heat_mat.shape[2], 1.0 / heat_mat.shape[1])
-            )
+                rescale=(1.0 / heat_mat.shape[2], 1.0 / heat_mat.shape[1]))
 
             pairs_by_conn.extend(pairs)
 
@@ -173,37 +180,56 @@ class PoseEstimator:
                 break
 
         # reject by subset count
-        humans = [human for human in humans if human.part_count() >= PoseEstimator.PAF_Count_Threshold]
+        humans = [
+            human for human in humans
+            if human.part_count() >= PoseEstimator.PAF_Count_Threshold
+        ]
 
         # reject by subset max score
-        humans = [human for human in humans if human.get_max_score() >= PoseEstimator.Part_Score_Threshold]
+        humans = [
+            human for human in humans
+            if human.get_max_score() >= PoseEstimator.Part_Score_Threshold
+        ]
 
         return humans
 
     @staticmethod
-    def score_pairs(part_idx1, part_idx2, coord_list1, coord_list2, paf_mat_x, paf_mat_y, heatmap, rescale=(1.0, 1.0)):
+    def score_pairs(part_idx1,
+                    part_idx2,
+                    coord_list1,
+                    coord_list2,
+                    paf_mat_x,
+                    paf_mat_y,
+                    heatmap,
+                    rescale=(1.0, 1.0)):
         connection_temp = []
 
         cnt = 0
         for idx1, (y1, x1) in enumerate(zip(coord_list1[0], coord_list1[1])):
-            for idx2, (y2, x2) in enumerate(zip(coord_list2[0], coord_list2[1])):
-                score, count = PoseEstimator.get_score(x1, y1, x2, y2, paf_mat_x, paf_mat_y)
+            for idx2, (y2, x2) in enumerate(
+                    zip(coord_list2[0], coord_list2[1])):
+                score, count = PoseEstimator.get_score(x1, y1, x2, y2,
+                                                       paf_mat_x, paf_mat_y)
                 cnt += 1
                 if count < PoseEstimator.PAF_Count_Threshold or score <= 0.0:
                     continue
-                connection_temp.append(PoseEstimator.PartPair(
-                    score=score,
-                    part_idx1=part_idx1, part_idx2=part_idx2,
-                    idx1=idx1, idx2=idx2,
-                    coord1=(x1 * rescale[0], y1 * rescale[1]),
-                    coord2=(x2 * rescale[0], y2 * rescale[1]),
-                    score1=heatmap[part_idx1][y1][x1],
-                    score2=heatmap[part_idx2][y2][x2],
-                ))
+                connection_temp.append(
+                    PoseEstimator.PartPair(
+                        score=score,
+                        part_idx1=part_idx1,
+                        part_idx2=part_idx2,
+                        idx1=idx1,
+                        idx2=idx2,
+                        coord1=(x1 * rescale[0], y1 * rescale[1]),
+                        coord2=(x2 * rescale[0], y2 * rescale[1]),
+                        score1=heatmap[part_idx1][y1][x1],
+                        score2=heatmap[part_idx2][y2][x2],
+                    ))
 
         connection = []
         used_idx1, used_idx2 = set(), set()
-        for candidate in sorted(connection_temp, key=lambda x: x.score, reverse=True):
+        for candidate in sorted(
+                connection_temp, key=lambda x: x.score, reverse=True):
             # check not connected
             if candidate.idx1 in used_idx1 or candidate.idx2 in used_idx2:
                 continue
@@ -218,15 +244,17 @@ class PoseEstimator:
         __num_inter = 10
         __num_inter_f = float(__num_inter)
         dx, dy = x2 - x1, y2 - y1
-        normVec = math.sqrt(dx ** 2 + dy ** 2)
+        normVec = math.sqrt(dx**2 + dy**2)
 
         if normVec < 1e-4:
             return 0.0, 0
 
         vx, vy = dx / normVec, dy / normVec
 
-        xs = np.arange(x1, x2, dx / __num_inter_f) if x1 != x2 else np.full((__num_inter,), x1)
-        ys = np.arange(y1, y2, dy / __num_inter_f) if y1 != y2 else np.full((__num_inter,), y1)
+        xs = np.arange(x1, x2, dx / __num_inter_f) if x1 != x2 else np.full(
+            (__num_inter, ), x1)
+        ys = np.arange(y1, y2, dy / __num_inter_f) if y1 != y2 else np.full(
+            (__num_inter, ), y1)
         xs = (xs + 0.5).astype(np.int8)
         ys = (ys + 0.5).astype(np.int8)
 
@@ -248,7 +276,7 @@ class PoseEstimator:
 
 
 class TfPoseEstimator:
-    ENSEMBLE = 'addup'        # average, addup
+    ENSEMBLE = 'addup'  # average, addup
 
     def __init__(self, graph_path, target_size=(320, 240)):
         self.target_size = target_size
@@ -265,8 +293,10 @@ class TfPoseEstimator:
         # for op in self.graph.get_operations():
         #     print(op.name)
 
-        self.tensor_image = self.graph.get_tensor_by_name('TfPoseEstimator/image:0')
-        self.tensor_output = self.graph.get_tensor_by_name('TfPoseEstimator/Openpose/concat_stage7:0')
+        self.tensor_image = self.graph.get_tensor_by_name(
+            'TfPoseEstimator/image:0')
+        self.tensor_output = self.graph.get_tensor_by_name(
+            'TfPoseEstimator/Openpose/concat_stage7:0')
 
         self.heatMat = self.pafMat = None
 
@@ -274,9 +304,12 @@ class TfPoseEstimator:
         self.persistent_sess.run(
             self.tensor_output,
             feed_dict={
-                self.tensor_image: [np.ndarray(shape=(target_size[1], target_size[0], 3), dtype=np.float32)]
-            }
-        )
+                self.tensor_image: [
+                    np.ndarray(
+                        shape=(target_size[1], target_size[0], 3),
+                        dtype=np.float32)
+                ]
+            })
 
     def __del__(self):
         self.persistent_sess.close()
@@ -302,17 +335,27 @@ class TfPoseEstimator:
                     continue
 
                 body_part = human.body_parts[i]
-                center = (int(body_part.x * image_w + 0.5), int(body_part.y * image_h + 0.5))
+                center = (int(body_part.x * image_w + 0.5),
+                          int(body_part.y * image_h + 0.5))
                 centers[i] = center
-                cv2.circle(npimg, center, 3, common.CocoColors[i], thickness=3, lineType=8, shift=0)
+                cv2.circle(
+                    npimg,
+                    center,
+                    3,
+                    common.CocoColors[i],
+                    thickness=3,
+                    lineType=8,
+                    shift=0)
 
             # draw line
             for pair_order, pair in enumerate(common.CocoPairsRender):
-                if pair[0] not in human.body_parts.keys() or pair[1] not in human.body_parts.keys():
+                if pair[0] not in human.body_parts.keys(
+                ) or pair[1] not in human.body_parts.keys():
                     continue
 
                 #npimg = cv2.line(npimg, centers[pair[0]], centers[pair[1]], common.CocoColors[pair_order], 3)
-                cv2.line(npimg, centers[pair[0]], centers[pair[1]], common.CocoColors[pair_order], 3)
+                cv2.line(npimg, centers[pair[0]], centers[pair[1]],
+                         common.CocoColors[pair_order], 3)
 
         return npimg
 
@@ -332,7 +375,8 @@ class TfPoseEstimator:
             ratio_x = (1. - self.target_size[0] / float(npimg.shape[1])) / 2.0
             ratio_y = (1. - self.target_size[1] / float(npimg.shape[0])) / 2.0
             roi = self._crop_roi(npimg, ratio_x, ratio_y)
-            return [roi], [(ratio_x, ratio_y, 1.-ratio_x*2, 1.-ratio_y*2)]
+            return [roi], [(ratio_x, ratio_y, 1. - ratio_x * 2,
+                            1. - ratio_y * 2)]
         elif isinstance(scale, tuple) and len(scale) == 2:
             # scaling with sliding window : (scale, step)
             base_scale = get_base_scale(scale[0], img_w, img_h)
@@ -342,8 +386,9 @@ class TfPoseEstimator:
             window_step = scale[1]
             rois = []
             infos = []
-            for ratio_x, ratio_y in itertools.product(np.arange(0., 1.01 - base_scale_w, window_step),
-                                                      np.arange(0., 1.01 - base_scale_h, window_step)):
+            for ratio_x, ratio_y in itertools.product(
+                    np.arange(0., 1.01 - base_scale_w, window_step),
+                    np.arange(0., 1.01 - base_scale_h, window_step)):
                 roi = self._crop_roi(npimg, ratio_x, ratio_y)
                 rois.append(roi)
                 infos.append((ratio_x, ratio_y, base_scale_w, base_scale_h))
@@ -371,22 +416,26 @@ class TfPoseEstimator:
     def _crop_roi(self, npimg, ratio_x, ratio_y):
         target_w, target_h = self.target_size
         h, w = npimg.shape[:2]
-        x = max(int(w*ratio_x-.5), 0)
-        y = max(int(h*ratio_y-.5), 0)
-        cropped = npimg[y:y+target_h, x:x+target_w]
+        x = max(int(w * ratio_x - .5), 0)
+        y = max(int(h * ratio_y - .5), 0)
+        cropped = npimg[y:y + target_h, x:x + target_w]
 
         cropped_h, cropped_w = cropped.shape[:2]
         if cropped_w < target_w or cropped_h < target_h:
-            npblank = np.zeros((self.target_size[1], self.target_size[0], 3), dtype=np.uint8)
+            npblank = np.zeros(
+                (self.target_size[1], self.target_size[0], 3), dtype=np.uint8)
 
-            copy_x, copy_y = (target_w - cropped_w) // 2, (target_h - cropped_h) // 2
-            npblank[copy_y:copy_y+cropped_h, copy_x:copy_x+cropped_w] = cropped
+            copy_x, copy_y = (target_w - cropped_w) // 2, (
+                target_h - cropped_h) // 2
+            npblank[copy_y:copy_y + cropped_h, copy_x:
+                    copy_x + cropped_w] = cropped
         else:
             return cropped
 
     def inference(self, npimg, scales=None):
         if npimg is None:
-            raise Exception('The image is not valid. Please check your image exists.')
+            raise Exception(
+                'The image is not valid. Please check your image exists.')
 
         if not isinstance(scales, list):
             scales = [None]
@@ -408,7 +457,8 @@ class TfPoseEstimator:
             infos.extend(info)
 
         logger.debug('inference+')
-        output = self.persistent_sess.run(self.tensor_output, feed_dict={self.tensor_image: rois})
+        output = self.persistent_sess.run(
+            self.tensor_output, feed_dict={self.tensor_image: rois})
         heatMats = output[:, :, :, :19]
         pafMats = output[:, :, :, 19:]
         logger.debug('inference-')
@@ -418,27 +468,37 @@ class TfPoseEstimator:
         for info in infos:
             max_ratio_w = min(max_ratio_w, info[2])
             max_ratio_h = min(max_ratio_h, info[3])
-        mat_w, mat_h = int(output_w/max_ratio_w), int(output_h/max_ratio_h)
+        mat_w, mat_h = int(output_w / max_ratio_w), int(output_h / max_ratio_h)
         resized_heatMat = np.zeros((mat_h, mat_w, 19), dtype=np.float32)
         resized_pafMat = np.zeros((mat_h, mat_w, 38), dtype=np.float32)
         resized_cntMat = np.zeros((mat_h, mat_w, 1), dtype=np.float32)
         resized_cntMat += 1e-12
 
         for heatMat, pafMat, info in zip(heatMats, pafMats, infos):
-            w, h = int(info[2]*mat_w), int(info[3]*mat_h)
+            w, h = int(info[2] * mat_w), int(info[3] * mat_h)
             heatMat = cv2.resize(heatMat, (w, h))
             pafMat = cv2.resize(pafMat, (w, h))
             x, y = int(info[0] * mat_w), int(info[1] * mat_h)
 
             if TfPoseEstimator.ENSEMBLE == 'average':
                 # average
-                resized_heatMat[max(0, y):y + h, max(0, x):x + w, :] += heatMat[max(0, -y):, max(0, -x):, :]
-                resized_pafMat[max(0,y):y+h, max(0, x):x+w, :] += pafMat[max(0, -y):, max(0, -x):, :]
-                resized_cntMat[max(0,y):y+h, max(0, x):x+w, :] += 1
+                resized_heatMat[max(0, y):y + h,
+                                max(0, x):x + w, :] += heatMat[max(0, -y):,
+                                                               max(0, -x):, :]
+                resized_pafMat[max(0, y):y + h,
+                               max(0, x):x + w, :] += pafMat[max(0, -y):,
+                                                             max(0, -x):, :]
+                resized_cntMat[max(0, y):y + h, max(0, x):x + w, :] += 1
             else:
                 # add up
-                resized_heatMat[max(0, y):y + h, max(0, x):x + w, :] = np.maximum(resized_heatMat[max(0, y):y + h, max(0, x):x + w, :], heatMat[max(0, -y):, max(0, -x):, :])
-                resized_pafMat[max(0,y):y+h, max(0, x):x+w, :] += pafMat[max(0, -y):, max(0, -x):, :]
+                resized_heatMat[max(
+                    0, y):y + h, max(0, x):x + w, :] = np.maximum(
+                        resized_heatMat[max(0, y):y + h,
+                                        max(0, x):x + w, :],
+                        heatMat[max(0, -y):, max(0, -x):, :])
+                resized_pafMat[max(0, y):y + h,
+                               max(0, x):x + w, :] += pafMat[max(0, -y):,
+                                                             max(0, -x):, :]
                 resized_cntMat[max(0, y):y + h, max(0, x):x + w, :] += 1
 
         if TfPoseEstimator.ENSEMBLE == 'average':
