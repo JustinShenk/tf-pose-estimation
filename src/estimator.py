0c44a1afbfe73e77b2141d8c687edb7638f6685e
Justin Shenk <shenk.justin@gmail.com>
2018-05-13 16:09:06 +0200

    Fix duplicate logging

--- src/estimator.py
+++ src/estimator.py
@@ -14,9 +14,11 @@ from common import CocoPairsNetwork, CocoPairs, CocoPart
 logger = logging.getLogger('TfPoseEstimator')
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
-formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+formatter = logging.Formatter(
+    '[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
 ch.setFormatter(formatter)
-logger.addHandler(ch)
+
+# logger.addHandler(ch)
 
 
 class Human:
